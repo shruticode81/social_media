6cab6b7f5698ef775343132028b0273ddecd65e3
shruti anand <vitikaawesome81@gmail.com>
2020-11-08 13:54:45 +0530

    Distributing the code into partials

--- routes/comments.js
+++ routes/comments.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const passpost = require('passport');
+const passport = require('../config/passport-local-strategy');
 const commentsController=require('../controllers/comments_controller');
 
 router.post('/create',passpost.checkAuthentication,commentsController.create);
+router.get('/destroy/:id', passport.checkAuthentication,commentsController.destroy);
 module.exports=router;
